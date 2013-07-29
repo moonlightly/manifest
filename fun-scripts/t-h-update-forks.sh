8440889f51fd8d01a1f4fdaebfd17358de212d33
Matt Filetto <matt.filetto@gmail.com>
2013-07-28 22:08:01 -0700

    fun-scripts: update t-h-update-forks.sh for cm-10.2
    Change-Id: I0d940d58cad3c8ecadb22ae93cfaa5ba43220814

--- fun-scripts/t-h-update-forks.sh
+++ fun-scripts/t-h-update-forks.sh
@@ -3,69 +3,69 @@ echo updating forked repos...
 cd $HOME/android/system
 #cd $HOME/android/system/packages/apps/Camera
 #echo Updating packages/apps/Camera...
-####git checkout cm-10.1
+####git checkout cm-10.2
 #git fetch cm
 #echo Merging...
-#git merge cm/cm-10.1
+#git merge cm/cm-10.2
 cd $HOME/android/system/packages/apps/Phone
 echo Updating packages/apps/Phone...
-##git checkout cm-10.1
+##git checkout cm-10.2
 git fetch cm
 echo Merging...
-git merge cm/cm-10.1
+git merge cm/cm-10.2
 #cd $HOME/android/system/frameworks/av
 #echo Updating frameworks/av...
-##git checkout cm-10.1
+##git checkout cm-10.2
 #git fetch cm
 #echo Merging
-#git merge cm/cm-10.1
+#git merge cm/cm-10.2
 cd $HOME/android/system/frameworks/base
 echo Updating frameworks/base
-##git checkout cm-10.1
+##git checkout cm-10.2
 git fetch cm
 echo Merging...
-git merge cm/cm-10.1
+git merge cm/cm-10.2
 #cd $HOME/android/system/frameworks/native
 #echo Updating frameworks/native...
-##git checkout cm-10.1
+##git checkout cm-10.2
 #git fetch cm
 #echo Merging...
-#git merge cm/cm-10.1
-#cd $HOME/android/system/hardware/libhardware
-#echo Updating hardware/libhardware...
-##git checkout cm-10.1
-#git fetch cm
-#echo Merging...
-#git merge cm/cm-10.1
+#git merge cm/cm-10.2
+cd $HOME/android/system/hardware/libhardware
+echo Updating hardware/libhardware...
+##git checkout cm-10.2
+git fetch cm
+echo Merging...
+git merge cm/cm-10.2
 cd $HOME/android/system/android
 git fetch cm
 cd $HOME/android/system/build
 git fetch cm
-git merge cm/cm-10.1
+git merge cm/cm-10.2
 #cd $HOME/android/system/hardware/qcom/audio-caf
 #git fetch cm
-#git merge cm/cm-10.1
+#git merge cm/cm-10.2
 #cd $HOME/android/system/packages/apps/Gallery2
 #git fetch cm
-#git merge cm/cm-10.1
+#git merge cm/cm-10.2
 #cd $HOME/android/system/hardware/qcom/gps
 #git fetch cm
-#git merge cm/cm-10.1
+#git merge cm/cm-10.2
 #cd $HOME/android/system/hardware/msm7k
 #git fetch cm
-#git merge cm/cm-10.1
+#git merge cm/cm-10.2
 #cd $HOME/android/system/hardware/qcom/media
 #git fetch cm
-#git merge cm/cm-10.1
+#git merge cm/cm-10.2
 cd $HOME/android/system/hardware/libhardware_legacy
 git fetch cm
-git merge cm/cm-10.1
+git merge cm/cm-10.2
 #cd $HOME/android/system/system/core
 #git fetch cm
-#git merge cm/cm-10.1
+#git merge cm/cm-10.2
 cd $HOME/android/system/vendor/cm
 git fetch cm
-git merge cm/cm-10.1
+git merge cm/cm-10.2
 #
 echo update complete now opening logs to view changes...
 echo press "Q" to close log and move on to next...
